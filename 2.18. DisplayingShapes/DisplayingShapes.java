afd165a60abbbed238a3c91094591d9f96aa5f43
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2018-01-06 12:05:36 +0200

    2.18. DisplayingShapes

---
+++ 2.18. DisplayingShapes/DisplayingShapes.java
@@ -0,0 +1,32 @@
+/*
+
+         Exercise:  2.18
+         Filename:  DisplayingShapes.java
+		 
+      Description:  Write an application that displays a box, an oval, an arrow
+                    and a diamond using asterisks (*)
+
+           Author:  Eng.Fady Fawzy
+          Version:  1.0
+
+          Created:  11/8/2016
+    Latest Update:  
+         Revision:  30/8/2016
+
+*/
+
+public class DisplayingShapes
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "*********     ***       *        *" );
+		System.out.println( "*       *   *     *    ***      * *" );
+		System.out.println( "*       *  *       *  *****    *   *" );
+		System.out.println( "*       *  *       *    *     *     *" );
+		System.out.println( "*       *  *       *    *    *       *" );
+		System.out.println( "*       *  *       *    *     *     *" );
+		System.out.println( "*       *  *       *    *      *   *" );
+		System.out.println( "*       *   *     *     *       * *" );
+		System.out.println( "*********     ***       *        *" );
+	}
+}
