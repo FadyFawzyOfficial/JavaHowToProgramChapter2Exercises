af1b9adacc45e388a3bd3e1e7ecd040cf7aa36ee
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2018-01-10 12:09:06 +0200

    2.27. Checkerboard

---
+++ 2.27. Checkerboard/Checkerboard.java
@@ -0,0 +1,32 @@
+
+
+/*
+
+         Exercise:  2.27
+         Filename:  Checkerboard.java
+		 
+      Description:  Write an application that displays a checkerboard pattern
+
+           Author:  Eng.Fady Fawzy
+          Version:  1.0 and 1.1
+
+          Created:  18/8/2016
+    Latest Update:  
+         Revision:  18/8/2016
+
+*/
+
+public class Checkerboard
+{
+	public static void main( String[] args )
+	{
+		System.out.print( " * * * * * * * *\n" );
+		System.out.print( "  * * * * * * * *\n" );
+		System.out.print( " * * * * * * * *\n" );
+		System.out.print( "  * * * * * * * *\n" );
+		System.out.print( " * * * * * * * *\n" );
+		System.out.print( "  * * * * * * * *\n" );
+		System.out.print( " * * * * * * * *\n" );
+		System.out.print( "  * * * * * * * *\n" );
+	}
+}
