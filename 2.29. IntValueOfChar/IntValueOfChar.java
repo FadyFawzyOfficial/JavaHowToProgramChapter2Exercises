ab01cf5c57add552884267c396b1e57713482a75
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2018-01-12 12:11:45 +0200

    2.29. IntValueOfChar

---
+++ 2.29. IntValueOfChar/IntValueOfChar.java
@@ -0,0 +1,45 @@
+
+
+/*
+
+         Exercise:  2.29
+         Filename:  IntValueOfChar.java
+		 
+      Description:  write an application that displays the integer equivalents of 
+                    some uppercase letters, lowercase letters, digits and special symbols.
+
+           Author:  Eng.Fady Fawzy
+          Version:  1.0
+
+          Created:  16/8/2016
+    Latest Update:  
+         Revision:  30/8/2016
+
+*/
+
+public class IntValueOfChar
+{
+    public static void main ( String[]args )
+    {
+        System.out.println( "Welcome to Integer Value of a Character application" );
+	    System.out.printf( "The character %c has the value %d\n", 'A', ( (int) 'A' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'A', ( (int) 'A' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'A', ( (int) 'A' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'A', ( (int) 'A' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'A', ( (int) 'A' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'A', ( (int) 'A' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'B', ( (int) 'B' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'C', ( (int) 'C' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'a', ( (int) 'a' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'b', ( (int) 'b' ) );
+	    System.out.printf( "The character %c has the value %d\n", 'c', ( (int) 'c' ) );
+	    System.out.printf( "The character %c has the value %d\n", '0', ( (int) '0' ) );
+	    System.out.printf( "The character %c has the value %d\n", '1', ( (int) '1' ) );
+	    System.out.printf( "The character %c has the value %d\n", '2', ( (int) '2' ) );
+	    System.out.printf( "The character %c has the value %d\n", '$', ( (int) '$' ) );
+	    System.out.printf( "The character %c has the value %d\n", '*', ( (int) '*' ) );
+	    System.out.printf( "The character %c has the value %d\n", '+', ( (int) '+' ) );
+	    System.out.printf( "The character %c has the value %d\n", '/', ( (int) '/' ) );
+	    System.out.printf( "The character %c has the value %d\n", ' ', ( (int) ' ' ) );
+    }
+}
