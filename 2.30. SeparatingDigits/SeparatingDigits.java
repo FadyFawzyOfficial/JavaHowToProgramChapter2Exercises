8cb7703f322a7127745b9d84f36491d9eabc472e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2018-01-13 12:16:21 +0200

    2.30. SeparatingDigits

--- 2.30. SeparatingDigits/SeparatingDigits.java
+++ 2.30. SeparatingDigits/SeparatingDigits.java
@@ -10,33 +10,31 @@ import java.util.Scanner;
                     separated from one another by three spaces each.
 
            Author:  Eng.Fady Fawzy
-          Version:  1.0
+          Version:  1.1
 
           Created:  11/8/2016
-    Latest Update:  
+    Latest Update:  30/8/2016
          Revision:  30/8/2016
 
 */
 
 public class SeparatingDigits
 {
-   public static void main( String[] args )
-   {
-       // Create Scanner to obtain input from user
-       Scanner input = new Scanner( System.in );
-       
-       System.out.println( "Welcome to Separating the Digits in an Integer application" );
-       System.out.print( "Please, Enter number consisting of five digits: " ); // prompt for input
-       int number = input.nextInt();
-       int digit1, digit2, digit3, digit4, digit5;
-       
-       digit1 = number / 10000;
-       digit2 = number % 10000 / 1000;
-       digit3 = number % 10000 % 1000 / 100;
-       digit4 = number % 10000 % 1000 % 100 / 10;
-       digit5 = number % 10000 % 1000 % 100 % 10 / 1;
-       
-       System.out.printf( "%d  %d  %d  %d  %d\n",
-               digit1, digit2, digit3, digit4, digit5 );
-   } // end method main
+    public static void main( String[] args )
+    {
+        // Create Scanner to obtain input from user
+        Scanner input = new Scanner( System.in );
+        
+        System.out.println( "Welcome to Separating the Digits in an Integer application" );
+        System.out.print( "Please, Enter number consisting of five digits: " ); // prompt for input
+        int number = input.nextInt();
+        
+        int x1 = number % 10000;
+        int x2 = x1 % 1000;
+        int x3 = x2 %100;
+        int x4 = x3 %10;
+        
+        System.out.printf( " %d   %d   %d   %d   %d\n",
+                number / 10000, x1 / 1000, x2 / 100, x3 / 10, x4 / 1 );
+    } // end method main
 } // end class SeparatingDigits
